239638b3e03727cdd400810ab0cc061143018c07
Princeone <146822722+p1princeone@users.noreply.github.com>
2024-01-03 15:48:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split("94774071805,")
+global.owner = process.env.OWNER_NUMBER.split("254793651678,")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'sam@secktor.live'
@@ -11,20 +11,20 @@ global.blockJids = process.env.BLOCK_JID ||'120363165030810524@g.us' ;
 global.allowJids = process.env.ALLOW_JID ||'120363165030810524@g.us' ;
 global.github = 'https://github.com/kaveesha-sithum/Dark-Nero-Bot-'
 global.location = 'Sri Lanka'
-global.gurl = 'https://instagram.com/' // add your username
+global.gurl = 'https://instagram.com/p1princeone' // add your username
 global.sudo = process.env.SUDO || '94774071805'
 global.devs = '94774071805';
 global.website = 'https://kaveeshasithum.netlify.app/'
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.ibb.co/zrvByTb/IMG-20230609-WA0083.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? 'DARK-NERO-MD 💗' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'CYBER-YAKUZA' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? '℘ཞıŋƈɛ◎ŋɛ 💗' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'Princeone' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_INFO.split(";")[0] === undefined ? '💗🎵DARK NERO:CYBER-YAKUZA' : process.env.PACK_INFO.split(";")[0],
-  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? '💗🎵DARK NERO:PRINCEONE' : process.env.PACK_INFO.split(";")[0],
+  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? true : process.env.AUTO_READ_STATUS,
   packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'DARKNERO-MD' : process.env.PACK_INFO.split(";")[1],	
   dl_limit: process.env.DL_SIZE || '350',
-  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
+  autoreaction:  process.env.AUTO_REACTION  === undefined ? true : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? true : process.env.ALWAYS_ONLINE,
   antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
